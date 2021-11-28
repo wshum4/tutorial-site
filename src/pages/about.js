d1b341b36f9c4db6eeeb34a24c8614a796dd288e
wshum <wince.shum@gmail.com>
2021-11-28 16:49:42 -0500

    layout and link component

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="about">
+      <p>more info here</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
